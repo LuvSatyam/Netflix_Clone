1aa1ca61bc5c9364e960b475351cededc0eda253
Luv Satyam <luvraghav328@gmail.com>
2024-04-23 00:38:12 +0530

    improved javascript concept

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 let btns=document.querySelectorAll(".container");
 let hiddenContent=document.querySelectorAll(".hidden-content");
+let crossSign=document.querySelector("svg");
 let i,reset=true,val=true;
 
 
 const hide=(i)=>{
     if(reset){
-        hiddenContent[i].style.display="flex";
+        hiddenContent[i].style.display="flex";      
+        btns[i].childNodes[3].style.display="flex";
+        btns[i].childNodes[1].style.display="none";
         reset=false;
     }
     else{
-        hiddenContent[i].style.display="none";
+        hiddenContent[i].style.display="none";  
+        btns[i].childNodes[3].style.display="none";
+        btns[i].childNodes[1].style.display="flex";
         reset=true;
-    }
+     }
 
 }
 
@@ -19,17 +24,12 @@ const hide=(i)=>{
 
 for(let btn of btns){
     btn.addEventListener("click",function open(){
-        console.log("clicked");
-        
-      
         let nodeListArray = Array.from(btns);
         let index = nodeListArray.indexOf(btn);
         hide(index);
-
-
-        
-      
     })
 }
+
+
 
 
